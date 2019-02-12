636f5998935a568272a8bd57dd2a5fb400b7af9c
Moti Bartov <motib@tikalk.com>
2019-02-12 13:58:15 +0200

    Voice recognition

--- android/app/src/main/kotlin/com/tikalk/moveshuttle/MainActivity.kt
+++ android/app/src/main/kotlin/com/tikalk/moveshuttle/MainActivity.kt
@@ -1,13 +1,47 @@
 package com.tikalk.moveshuttle
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
+import com.google.android.gms.actions.NoteIntents
 
-class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
+import io.flutter.plugin.common.MethodChannel
+
+class MainActivity : FlutterActivity() {
+
+    companion object {
+        const val TAG  = "MainActivity"
+    }
+    var savedNote: String? = null
+    lateinit var channel : MethodChannel
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(this)
+        Log.i(TAG, "onCreate:")
+
+        val intent = intent
+        val action = intent.action
+        val type = intent.type
+        Log.i(TAG, "onCreate: ${intent.action}")
+
+//        if (NoteIntents.ACTION_CREATE_NOTE == action && type != null) {
+//            if ("text/plain" == type) {
+//                savedNote = intent.getStringExtra(Intent.EXTRA_TEXT)
+//            }
+//        }
+//
+//        channel = MethodChannel(flutterView, "app.channel.shared.data")
+//
+//        channel.setMethodCallHandler { methodCall, result ->
+//            Log.i(TAG,"setMethodCallHandler: ")
+//            if (methodCall.method.contentEquals("getSavedNote")) {
+//                result?.success(savedNote)
+//                savedNote = null
+//            }
+//        }
+    }
+
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_dialogflow/dialogflow_v2.dart';
 import 'package:tts/tts.dart';
+import 'package:speech_recognition/speech_recognition.dart';
 
 void main() => runApp(new MyApp());
 
@@ -33,6 +35,11 @@ class _MyHomePageState extends State<MyHomePage> {
 
   final List<ChatMessage> _messages = <ChatMessage>[];
   final TextEditingController _textController = new TextEditingController();
+  var _speech;
+  bool _speechRecognitionAvailable;
+  String _currentLocale;
+  bool _isListening;
+  String transcription;
 
   Widget _buildTextComposer() {
     return new IconTheme(
@@ -92,11 +99,60 @@ class _MyHomePageState extends State<MyHomePage> {
     Response(text);
   }
 
+  void _handleFabClick(){
+    print("_handleFabClick: pressed");
+    _speech = SpeechRecognition();
+
+// The flutter app not only call methods on the host platform,
+// it also needs to receive method calls from host.
+    _speech.setAvailabilityHandler((bool result)
+    => setState(() => _speechRecognitionAvailable = result));
+
+// handle device current locale detection
+    _speech.setCurrentLocaleHandler((String locale) =>
+        setState((){
+          print("current locale: $locale");
+            _currentLocale = locale;
+        }));
+
+    _speech.setRecognitionStartedHandler(() => setState((){
+      _isListening = true;
+      print("csetRecognitionStartedHandler: isListening $_isListening");
+    });
+
+// this handler will be called during recognition.
+// the iOS API sends intermediate results,
+// On my Android device, only the final transcription is received
+    _speech.setRecognitionResultHandler((String text) => setState((){
+          print("setRecognitionResultHandler: $text");
+          transcription = text;
+    }));
+
+    _speech.setRecognitionCompleteHandler(() => setState((){
+    _isListening = false;
+    } ));
+
+// 1st launch : speech recognition permission / initialization
+    _speech.activate().then((res) => setState(() => _speechRecognitionAvailable = res));
+
+    _speech.listen(locale:_currentLocale).then((result)=>
+        print('result : $result')
+    );
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text(widget.title),
+      ),
+      floatingActionButton: Container(
+        padding: EdgeInsets.only(bottom: 50.0, right: 25),
+        child: Stack(children: <Widget>[
+          FloatingActionButton(onPressed: _handleFabClick, child: Icon(Icons.keyboard_voice))
+          ]
+      )
       ),
       body: new Column(children: <Widget>[
         new Flexible(
@@ -113,6 +169,29 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ]),
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+//    _getSavedWeightNote();
+  }
+
+  Future<double> _getSavedWeightNote() async {
+    String sharedData = await const MethodChannel('app.channel.shared.data')
+        .invokeMethod("getSavedNote");
+    print("_getSavedWeightNote: $sharedData");
+
+    if (sharedData != null) {
+      int firstIndex = sharedData.indexOf(new RegExp("[0-9]"));
+      int lastIndex = sharedData.lastIndexOf(new RegExp("[0-9]"));
+      if (firstIndex != -1) {
+        String number = sharedData.substring(firstIndex, lastIndex + 1);
+        double num = double.parse(number, (error) => null);
+        return num;
+      }
+    }
+    return null;
   }
 }
 
