df8c4dca428dab915fb16d5c75c348b2309b325b
Elnatan Derech <>
2019-02-12 12:43:05 +0200

    add form fields

--- lib/main.dart
+++ lib/main.dart
@@ -30,42 +30,55 @@ class MyHomePage extends StatefulWidget {
 /* https://github.com/VictorRancesCode/flutter_dialogflow/blob/master/example/lib/main.dart */
 
 class _MyHomePageState extends State<MyHomePage> {
-
   final List<ChatMessage> _messages = <ChatMessage>[];
   final TextEditingController _textController = new TextEditingController();
 
   Widget _buildTextComposer() {
-    return new IconTheme(
-      data: new IconThemeData(color: Theme.of(context).accentColor),
-      child: new Container(
-        margin: const EdgeInsets.symmetric(horizontal: 8.0),
-        child: new Row(
-          children: <Widget>[
-            new Flexible(
-              child: new TextField(
-                controller: _textController,
-                onSubmitted: _handleSubmitted,
-                decoration:
-                new InputDecoration.collapsed(hintText: "Send a message"),
+    return new Column(children: <Widget>[
+      new IconTheme(
+        data: new IconThemeData(color: Theme.of(context).accentColor),
+        child: new Container(
+          margin: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: new Row(
+            children: <Widget>[
+              new Flexible(
+                child: new TextField(
+                  controller: _textController,
+                  onSubmitted: _handleSubmitted,
+                  decoration:
+                      new InputDecoration.collapsed(hintText: "Send a message"),
+                ),
               ),
-            ),
-            new Container(
-              margin: new EdgeInsets.symmetric(horizontal: 4.0),
-              child: new IconButton(
-                  icon: new Icon(Icons.send),
-                  onPressed: () => _handleSubmitted(_textController.text)),
-            ),
-          ],
+              new Container(
+                margin: new EdgeInsets.symmetric(horizontal: 4.0),
+                child: new IconButton(
+                    icon: new Icon(Icons.send),
+                    onPressed: () => _handleSubmitted(_textController.text)),
+              ),
+            ],
+          ),
         ),
       ),
-    );
+      RaisedButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => SecondRoute()),
+          );
+        },
+        child: Text("Add Shattle"),
+      ),
+    ]);
   }
 
   void Response(query) async {
     _textController.clear();
     // TODO update credentials here
-    AuthGoogle authGoogle = await AuthGoogle(fileJson: "assets/fuseteam3-1c284d1e865d.json").build();
-    Dialogflow dialogflow = Dialogflow(authGoogle: authGoogle, language: Language.ENGLISH);
+    AuthGoogle authGoogle =
+        await AuthGoogle(fileJson: "assets/fuseteam3-1c284d1e865d.json")
+            .build();
+    Dialogflow dialogflow =
+        Dialogflow(authGoogle: authGoogle, language: Language.ENGLISH);
     AIResponse response = await dialogflow.detectIntent(query);
 
     ChatMessage message = new ChatMessage(
@@ -101,11 +114,11 @@ class _MyHomePageState extends State<MyHomePage> {
       body: new Column(children: <Widget>[
         new Flexible(
             child: new ListView.builder(
-              padding: new EdgeInsets.all(8.0),
-              reverse: true,
-              itemBuilder: (_, int index) => _messages[index],
-              itemCount: _messages.length,
-            )),
+          padding: new EdgeInsets.all(8.0),
+          reverse: true,
+          itemBuilder: (_, int index) => _messages[index],
+          itemCount: _messages.length,
+        )),
         new Divider(height: 1.0),
         new Container(
           decoration: new BoxDecoration(color: Theme.of(context).cardColor),
@@ -133,7 +146,8 @@ class ChatMessage extends StatelessWidget {
         child: new Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            new Text(this.name, style:new TextStyle(fontWeight:FontWeight.bold )),
+            new Text(this.name,
+                style: new TextStyle(fontWeight: FontWeight.bold)),
             new Container(
               margin: const EdgeInsets.only(top: 5.0),
               child: new Text(text),
@@ -173,6 +187,49 @@ class ChatMessage extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: this.type ? myMessage(context) : otherMessage(context),
       ),
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final _fromController = TextEditingController();
+    final _toController = TextEditingController();
+    final _whenController = TextEditingController();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Route"),
+      ),
+      body: Center(
+          child: Column(
+        children: <Widget>[
+          TextFormField(
+            controller: _fromController,
+            style:
+                new TextStyle(fontSize: 20.0, height: 2.0, color: Colors.black),
+            decoration: new InputDecoration.collapsed(hintText: "From:"),
+          ),
+          TextFormField(
+            controller: _toController,
+            style:
+                new TextStyle(fontSize: 20.0, height: 2.0, color: Colors.black),
+            decoration: new InputDecoration.collapsed(hintText: "To:"),
+          ),
+          TextFormField(
+            controller: _whenController,
+            style:
+                new TextStyle(fontSize: 20.0, height: 2.0, color: Colors.black),
+            decoration: new InputDecoration.collapsed(hintText: "When:"),
+          ),
+          RaisedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text('Go back!'),
+          ),
+        ],
+      )),
     );
   }
 }
